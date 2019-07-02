56845a554b0d021eb554f4d9edc4b160fa7050a5
Bogdan Koteluk <bogdan.koteluk@gmail.com>
2019-07-02 14:50:13 +0200

    Dodanie komentarza w klasie Zadanie

--- src/Zadanie.java
+++ src/Zadanie.java
@@ -1,5 +1,6 @@
 public class Zadanie {
     public static void main(String[] args) {
+        // Nauka GIT-a
         System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
